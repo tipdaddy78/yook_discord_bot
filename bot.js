e36f3bb6b0d5bfaef88c7dc273b8afe2242ab836
ch1pset <theslenderpsycho@gmail.com>
2019-01-05 14:22:07 -0600

    Revised code to use discord.js instead of discord.io

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,46 +8,40 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
+const bot = new Discord.Client();
+
+bot.on('ready', (evt) => {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Logged in as: ' + bot.user);
 });
 
-var mod_list = ["ch1pset"];
-bot.on('message', function (user, userID, channelID, message, evt) {
+var mod_list = ["ch1pset", "tipdaddy"];
+bot.on('message', msg => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (msg.content.substring(0, 1) == '!') {
+        var args = msg.content.substring(1).split(' ');
         var cmd = args[0];
         switch(cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+                msg.reply('Pong!');
             break;
             // Just add any case commands if you want to..
             case 'addmod':
-                if(mod_list.includes(user))
+                if(mod_list.includes(msg.author.username))
                     mod_list.push(args[1]);
             break;
             case 'removemod':
-                if(mod_list.includes(user))
+                if(mod_list.includes(msg.author.username))
                     mod_list.splice(mod_list.indexOf(args[1]), 1);
             break;
             case 'showmods':
-                bot.sendMessage({
-                    to: channelID,
-                    message: mod_list
-                })
+                msg.channel.send(mod_list);
             break;
+            case 'me':
+                msg.channel.send(msg.author.username);
          }
      }
 });
+bot.login(auth.token)
