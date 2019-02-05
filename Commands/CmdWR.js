a0fe86a54fd0d66ee94b3e812689c799c8c12611
ch1pset <theslenderpsycho@gmail.com>
2019-02-05 02:24:19 -0600

    Began work on CmdWR

--- Commands/CmdWR.js
+++ Commands/CmdWR.js
@@ -1,11 +1,35 @@
 const H = require('./header.js');
 const https = require('https');
+const YL = require('../Database/YLSRC.json');
 const SRC = 'https://speedrun.com/api/v1'
-const YL = 'm1zz5210';
-const ANY_W_FLIGHT = '7kjzy44d';
-
 
 class CmdWR extends H.Command
 {
+    constructor(args)
+    {
+        super();
+        this.category = args[0];
+    }
+    exit()
+    {
 
+    }
+    execute()
+    {
+
+    }
+    getWR(res)
+    {
+        let rawData = '';
+        let parsedData;
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+              parsedData = JSON.parse(rawData).data.runs[0];
+              retrieveData.emit('data', parsedData);
+            }
+            catch (e);
+        });
+    }
 }
