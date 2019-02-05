4903da6628e1af46d0c6fbfe506a902f733eb6eb
ch1pset <theslenderpsycho@gmail.com>
2019-02-05 02:23:37 -0600

    More testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,76 @@
-var https = require('https');
-var api = 'http://speedrun.com/api/v1';
-var yl = 'm1zz5210';
-var any_w_flight = '7kjzy44d';
-const {Console} = require('console');
-var console = new Console({stdout:process.stdout,stderr:process.error});
 
-https.get(`https://www.speedrun.com/api/v1/leaderboards/m1zz5210/category/7kjzy44d`, (res) => {
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-      const parsedData = JSON.parse(rawData);
-      console.log(JSON.stringify(parsedData, null, 2));
-    } catch (e) {
-      console.error(e.message);
+const event = require('events');
+const https = require('https');
+const stdio = {cin:process.stdin,cout:process.stdout};
+
+var YL = require('../Database/YLSRC.json');
+var SRC = 'https://www.speedrun.com/api/v1';
+const {Console} = require('console');
+
+console.log(`Fetching All Bosses`);
+
+
+var retrieveData = new event();
+
+
+function getWR(cat)
+{
+    let parsedData = null;
+    https.get(`${SRC}/leaderboards/${YL.id}/category/${YL.cat[cat]}`, (res) =>
+    {
+        let rawData = '';
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () =>
+        {
+            try
+            {
+                console.log('parsing data...')
+                parsedData = JSON.parse(rawData).data.runs[0].run.weblink;
+                retrieveData.emit('data', parsedData);
+            }
+            catch(e)
+            {
+                console.log(e);
+            }
+        });
+    });
+    // while(!req.finished);
+    // return parsedData.data.runs[0];
+}
+
+function input(chunk)
+{
+    let str = chunk.toString();
+    let regx = /\b[\w]+\b/g;
+    let array = output = [];
+    while((array = regx.exec(str)) !== null)
+    {
+        output.push(array[0]);
     }
-  });
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
+    return output;
+}
+
+retrieveData.on('data', (wr) => {
+    console.log(wr);
 });
+
+function readConsole()
+{
+    stdio.cin.on('readable', () =>
+    {
+        let chunk;
+        while((chunk = stdio.cin.read()) !== null)
+        {
+            let args = input(chunk);
+            switch(args[0])
+            {
+                case 'quit': process.exit(); break;
+                default:
+                getWR(args[0]);
+                break;
+            }
+        }
+    });
+}
+readConsole();
