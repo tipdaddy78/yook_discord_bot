42741ac990e19c73c229dbaf4c4f769f0089b2e8
ch1pset <theslenderpsycho@gmail.com>
2019-02-05 20:37:28 -0600

    Fully functional CmdWR implementation. May need to be expanded on later

--- Commands/CmdWR.js
+++ Commands/CmdWR.js
@@ -1,35 +1,51 @@
 const H = require('./header.js');
 const https = require('https');
 const YL = require('../Database/YLSRC.json');
-const SRC = 'https://speedrun.com/api/v1'
+const URL = 'https://www.speedrun.com/api/v1/leaderboards/m1zz5210/category'
 
-class CmdWR extends H.Command
+module.exports = class CmdWR extends H.Command
 {
     constructor(args)
     {
-        super();
-        this.category = args[0];
+        super('wr');
+        this.cat = args[0];
+        this.ch = 'ch';
+        H.Logger.info('CatID: ' + YL[this.cat]);
     }
-    exit()
+    exit(msg)
     {
-
+        return {ch:this.ch,msg:msg};
     }
-    execute()
+    async execute()
     {
-
+        let callback = arguments[2];
+        await this.getWR()
+        .then(data => callback(this.exit(data)))
+        .catch(e => callback(this.exit(this.message('notfound'))));
     }
-    getWR(res)
+    getWR()
     {
-        let rawData = '';
-        let parsedData;
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-              parsedData = JSON.parse(rawData).data.runs[0];
-              retrieveData.emit('data', parsedData);
-            }
-            catch (e);
+        return new Promise((resolve,reject) =>
+        {
+            https.get(`${URL}/${YL[this.cat]}`, res =>
+            {
+                let rawData = '';
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => { rawData += chunk; });
+                res.on('end', () =>
+                {
+                    try
+                    {
+                        H.Logger.info('Parsing...');
+                        resolve(JSON.parse(rawData).data.runs[0].run.weblink);
+                    }
+                    catch(e)
+                    {
+                        H.Logger.info(e);
+                        reject('Not found');
+                    }
+                });
+            });
         });
     }
 }
