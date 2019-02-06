5965da162bc75ee78bccadca470f9a3a6c2c348e
ch1pset <theslenderpsycho@gmail.com>
2019-02-05 23:35:12 -0600

    Added control over bot sessions with init, kill and restart commands

--- Bot/bot.js
+++ Bot/bot.js
@@ -8,47 +8,54 @@ const logger = require('../Logs/logger.js');
 const auth = require('./auth.json');
 
 // Initialize Discord Bot
-var bot = new Discord.Client();
+// var bot = new Discord.Client();
 var input = new MessageParser();
+var client = {};
 
-
-//Event listener for when bot successfully logs on
-bot.on('ready', (evt) =>
-    {
-        logger.info(`Connected: Logged in as: ${bot.user.username}  id: ${bot.user.id}`);
-    }
-);
-
-//Event listener for anytime a message is posted in a channel that our bot
-//has access to. Don't worry, I'm not logging everyone's messages to the bot.
-bot.on('message', msg =>
-    {
-        if(input.isCommand(msg.content[0]))
+function addBotListeners(bot)
+{
+    //Event listener for when bot successfully logs on
+    bot.on('ready', (evt) =>
         {
-            input.set(msg);
-            let cmd = input.command();
-            input.execute(cmd);
+            logger.info(`Connected: Logged in as: ${bot.user.username}  id: ${bot.user.id}`);
         }
-    }
-);
+    );
 
-// Create an event listener for new guild members
-bot.on('guildMemberAdd', member =>
+    //Event listener for anytime a message is posted in a channel that our bot
+    //has access to. Don't worry, I'm not logging everyone's messages to the bot.
+    bot.on('message', msg =>
+        {
+            if(input.isCommand(msg.content[0]))
+            {
+                input.set(msg);
+                let cmd = input.command();
+                input.execute(cmd);
+            }
+        }
+    );
+
+    // Create an event listener for new guild members
+    bot.on('guildMemberAdd', member =>
+        {
+            logger.info(`${member.displayName} has joined the ${member.guild.name} server!`)
+            // Send the message to a designated channel on a server:
+            const channel = member.guild.channels.find(ch => ch.name === 'newcomers' || ch.name === 'testing');
+            // Do nothing if the channel wasn't found on this server
+            if (!channel) return;
+            // Send the message, mentioning the member
+            channel.send(`Welcome to the server, ${member}!`);
+                        // + `I am INEPT, my creator is ch1pset.`
+                        // + `If you are interested in speedrunning YL, I have some useful commands you may use to find resources.`
+                        // + `Use !help for a list of commands and how to use them.`
+                        // + `To keep spam out of the channels, I will reply spammy searches in a DM`);
+        }
+    );
+
+    bot.on('disconnect', (e) =>
     {
-        logger.info(`${member.displayName} has joined the ${member.guild.name} server!`)
-        // Send the message to a designated channel on a server:
-        const channel = member.guild.channels.find(ch => ch.name === 'newcomers' || ch.name === 'testing');
-        // Do nothing if the channel wasn't found on this server
-        if (!channel) return;
-        // Send the message, mentioning the member
-        channel.send(`Welcome to the server, ${member}!`);
-                    // + `I am INEPT, my creator is ch1pset.`
-                    // + `If you are interested in speedrunning YL, I have some useful commands you may use to find resources.`
-                    // + `Use !help for a list of commands and how to use them.`
-                    // + `To keep spam out of the channels, I will reply spammy searches in a DM`);
-    }
-);
-
+        logger.info(`Client has disconnected. Type \'restart\' to begin a new session.`);
+    });
+}
 
 //Event listener for anytime a command is used. Output message gets sent to
 //proper channel here.
@@ -79,9 +86,15 @@ process.stdin.on('readable', () =>
     let chunk;
     while((chunk = process.stdin.read()) !== null)
     {
-        let cmd_in = /[\w]*\b/i.exec(chunk.toString());
+        let cmd_in = /[\w]*\b([\w]*\b)/i.exec(chunk.toString());
         switch(cmd_in? cmd_in[0]:null)
         {
+            case 'init': initBot(auth.token); break;
+            case 'restart':
+                killBot();
+                initBot(auth.token);
+            break;
+            case 'kill': killBot(); break;
             case 'mem':
             process.stdout.write(`${JSON.stringify(process.memoryUsage(), null, 2)}`);
             break;
@@ -89,6 +102,22 @@ process.stdin.on('readable', () =>
         }
     }
 })
+
+function initBot(token)
+{
+    client.bot = new Discord.Client();
+    addBotListeners(client.bot);
+    client.bot.login(token);
+}
+
+function killBot()
+{
+    if(client.bot)
+    {
+        client.bot.destroy();
+        logger.info(`Bot was destroyed`);
+    }
+}
 
 //Utility function to select output channel for sending messages from the bot.
 function sendToChannel(channel, data, output)
@@ -100,5 +129,3 @@ function sendToChannel(channel, data, output)
         case 'dm': data.usr.send(output); break;
     }
 }
-
-bot.login(auth.token);
